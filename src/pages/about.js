60a6a64901bfcde621d663d57a1fda58ab53a345
na-287 <100829062+nn-287@users.noreply.github.com>
2022-12-16 23:24:41 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,10 +54,7 @@ const AboutPage = (props) => {
             <br />
             <br />
             <p>
-              We created some of the world's first T-shirts and spent decades
-              perfecting the feel of the cotton. Today we are the only brand
-              that makes T-shirts in its own factory in the UK. And we do this
-              in the same factory we have occupied since 1937.
+              Hello World!
             </p>
           </div>
         </Container>
@@ -71,16 +68,7 @@ const AboutPage = (props) => {
             <h3>Our Values</h3>
             <div ref={valuesRef}>
               <p>
-                Sunspel produced some of the world's earliest T-shirts. In the
-                late 1800s the business made luxury tunics and undershirts from
-                lightweight Sea Island cotton for export to the Far East and
-                other warm climates. While these garments initially had silk
-                buttoned plackets, these were removed in the early 1900s and
-                replaced with simple bound necks to reduce manufacturing costs -
-                creating the T-shirt. We've supplied the world as the T-shirt
-                has evolved from underwear to outerwear, from symbol of youthful
-                rebellion to everyday wardrobe staple, and we've spent decades
-                refining its every last aspect.
+                Hello World
               </p>
               <ol>
                 <li>Be an ecowear</li>
